2172a1f72669e52cb7f8eaa4ba0805c53bc938ce
Rafael Maciel Vanat <rafaelvant@gmail.com>
2017-10-07 15:02:18 -0300

    Estrutura de métodos para reprodução do audio

--- wcm/widget/insight/src/main/webapp/resources/js/audio.js
+++ wcm/widget/insight/src/main/webapp/resources/js/audio.js
@@ -4,11 +4,18 @@
  * @param {Uint8Array} AudioStream 
  */
 var player = {
-    context : new AudioContext()
-    , play : function( bytes ) {
-        var buffer = new Uint8Array( bytes.length );
-        buffer.set( new Uint8Array(bytes), 0 );
-    
-        context.decodeAudioData(buffer.buffer, play);
+    context: new AudioContext(),
+    play: function (bytes) {
+        var buffer = new Uint8Array(bytes.length);
+        buffer.set(new Uint8Array(bytes), 0);
+
+        this.context.decodeAudioData(buffer.buffer, this._play);
+    },
+    _play(audioBuffer) {
+        //this aqui já é o audio context
+        var source = this.createBufferSource();
+        source.buffer = audioBuffer;
+        source.connect(this.destination);
+        source.start(0);
     }
 }
--- wcm/widget/insight/src/main/webapp/resources/js/insight.js
+++ wcm/widget/insight/src/main/webapp/resources/js/insight.js
@@ -3,27 +3,58 @@ var Insight = SuperWidget.extend({
     message: null,
 
     polly: {},
-    
+
     //método iniciado quando a widget é carregada
-    init: function() {
+    init: function () {
         AWS.config.region = 'us-east-1';
         AWS.config.update({
-            "accessKeyId": "XXX", //XXX: Não comitar as chaves carai
-            "secretAccessKey": "YYY",
+            "accessKeyId": "AKIAJJPFW2U6FQYWHJ2Q",
+            "secretAccessKey": "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",
         });
 
-        polly = new AWS.Polly({apiVersion: '2016-06-10'});
+        this.polly = new AWS.Polly({ apiVersion: '2016-06-10' });
+
+        this.informUser();
     },
 
     bindings: {
-        local: {
-            'show-message': ['click_showMessage']
+        // local: {
+        //     'show-message': ['click_showMessage']
+        // }
+    },
+
+    informUser: function(){
+        var params = this.getUserInsight();
+        if (params != null) {
+            this.polly.synthesizeSpeech(
+                params
+                , function (err, data) {
+                    if (err) {
+                        this.sorry();
+                        console.log(err, err.stack); // an error occurred
+                    }
+                    else player.play(data.AudioStream); // successful response
+                }
+            );
         }
     },
 
-    showMessage: function () {
-        $div = $('#helloMessage_' + this.instanceId);
-        $message = $('<div>').addClass('message').append(this.message);
-        $div.append($message);
+    getUserInsight: function () {
+        var params = {
+            OutputFormat: "mp3",
+            SampleRate: "8000",
+            Text: "O Vanat é o mais lindo do mundo",
+            TextType: "text",
+            VoiceId: "Vitoria"
+        };
+        return params;
+    },
+
+    sorry: function () {
+        FLUIGC.toast({
+            title: '',
+            message: 'Não tenho insight para você hoje :(',
+            type: 'info'
+        });
     }
 });
