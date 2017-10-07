8e314e10e4cfcae67985f757e3f069300f902217
Rafael Maciel Vanat <rafaelvant@gmail.com>
2017-10-07 15:03:43 -0300

    Revert "Estrutura de métodos para reprodução do audio"
    This reverts commit 2172a1f72669e52cb7f8eaa4ba0805c53bc938ce.

--- wcm/widget/insight/src/main/webapp/resources/js/audio.js
+++ wcm/widget/insight/src/main/webapp/resources/js/audio.js
@@ -4,18 +4,11 @@
  * @param {Uint8Array} AudioStream 
  */
 var player = {
-    context: new AudioContext(),
-    play: function (bytes) {
-        var buffer = new Uint8Array(bytes.length);
-        buffer.set(new Uint8Array(bytes), 0);
-
-        this.context.decodeAudioData(buffer.buffer, this._play);
-    },
-    _play(audioBuffer) {
-        //this aqui já é o audio context
-        var source = this.createBufferSource();
-        source.buffer = audioBuffer;
-        source.connect(this.destination);
-        source.start(0);
+    context : new AudioContext()
+    , play : function( bytes ) {
+        var buffer = new Uint8Array( bytes.length );
+        buffer.set( new Uint8Array(bytes), 0 );
+    
+        context.decodeAudioData(buffer.buffer, play);
     }
 }
